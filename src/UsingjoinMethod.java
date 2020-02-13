a13d81209a91964b41155f7e20070f5e0bbe0f31
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2020-02-13 09:36:27 +0530

    j

---
+++ src/UsingjoinMethod.java
@@ -0,0 +1,32 @@
+//using join method
+public class UsingjoinMethod {
+    public static void main(String[] args) {
+        strt t1 = new strt();
+        strt t2 = new strt();
+        strt t3 = new strt();
+        t1.start();
+        try {
+            //next thread will wait until t1 dies
+            t1.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        t2.start();
+        t3.start();
+    }
+}
+
+class strt extends Thread {
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            try {
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println(i + " " + Thread.currentThread().getName());
+
+        }
+    }
+}
