34517b2bd9c64257e8202eb459dc1dc8dcd4c05a
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2020-02-23 16:06:55 +0530

    j

---
+++ src/DemoClass.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(12);
+        int a = arr.get(0);
+
+
+    }
+}
