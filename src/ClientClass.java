93ea684d3f83f8de916572e1b605010b063c9c2f
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2020-02-28 20:09:09 +0530

    j

--- src/ClientClass.java
+++ src/ClientClass.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 public class ClientClass {
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("192.168.43.134", 6666);
+            Socket s = new Socket("192.168.137.1", 6666);
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
             dos.writeUTF("Hello from yash");
             dos.flush();
